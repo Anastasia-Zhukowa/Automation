c7f0ec83d31eeaef81ee521c4d7c331e2051fdc2
Anastasia-Zhukowa <152152200+Anastasia-Zhukowa@users.noreply.github.com>
2024-03-07 11:29:54 +0300

    first HW

---
+++ Lesson1/lesson_1_task_6.py
@@ -0,0 +1,31 @@
+def print_one(): # Вывести в консоль 1
+    print(1)
+def print_two():  # Вывести в консоль 2
+    print(2)
+
+def print_three():  # Вывести в консоль 3
+    print(3)
+
+def print_four():  # Вывести в консоль 4
+    print(4)
+
+def print_five():  # Вывести в консоль 5
+    print(5)
+
+def print_six():  # Вывести в консоль 6
+    print(6)
+
+def print_seven():  # Вывести в консоль 7
+    print(7)
+
+def print_eight():  # Вывести в консоль 8
+    print(8)
+
+def print_nine():  # Вывести в консоль 9
+    print(9)
+
+def print_zero():  # Вывести в консоль 0
+    print(0)
+
+
+print_eight(), print_eight(), print_zero(), print_zero(), print_five(), print_five(), print_five(), print_three(), print_five(), print_three(), print_five()
