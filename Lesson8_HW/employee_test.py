148bab875384d65dcbbb7f19ca9cc4b43ad618a9
Anastasia-Zhukowa <152152200+Anastasia-Zhukowa@users.noreply.github.com>
2024-05-19 20:59:59 +0300

    HW 8_исправленная_3

--- Lesson8_HW/employee_test.py
+++ Lesson8_HW/employee_test.py
@@ -1,10 +1,10 @@
 import pytest
 import requests
 from test_employee_api import Company
+from faker import Faker
 
-
+fake = Faker()
 api = Company("https://x-clients-be.onrender.com")
-
 
 def test_get_list_of_employees():
     name = "SkyPro"
@@ -14,43 +14,46 @@ def test_get_list_of_employees():
     employee_list = api.get_list_employee(new_id)
     assert len(employee_list) == 0
 
-
 def test_add_new_employee():
     name = "SkyPro"
     descr = "testing"
     company = api.create_company(name, descr)
     new_id = company["id"]
-    new_employee = api.add_new_employee(new_id, "Ivan", "Ivanov")
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    new_employee = api.add_new_employee(new_id, first_name, last_name)
     assert new_employee["id"] > 0
 
     resp = api.get_list_employee(new_id)
     assert resp[0]["companyId"] == new_id
-    assert resp[0]["firstName"] == "Ivan"
+    assert resp[0]["firstName"] == first_name
     assert resp[0]["isActive"] == True
-    assert resp[0]["lastName"] == "Ivanov"
-
+    assert resp[0]["lastName"] == last_name
 
 def test_get_employee_by_id():
     name = "SkyPro"
     descr = "testing"
     company = api.create_company(name, descr)
     new_id = company["id"]
-    new_employee = api.add_new_employee(new_id, "Ivan", "Testov")
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    new_employee = api.add_new_employee(new_id, first_name, last_name)
     id_employee = new_employee["id"]
     get_info = api.get_employee_by_id(id_employee)
-    assert get_info["firstName"] == "Ivan"
-    assert get_info["lastName"] == "Testov"
-
+    assert get_info["firstName"] == first_name
+    assert get_info["lastName"] == last_name
 
 def test_change_employee_info():
     name = "SkyPro"
     descr = "testing"
     company = api.create_company(name, descr)
     new_id = company["id"]
-    new_employee = api.add_new_employee(new_id, "Ivan", "Petrov")
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    new_employee = api.add_new_employee(new_id, first_name, last_name)
     id_employee = new_employee["id"]
 
-    update = api.update_employee_info(id_employee, "Petrov2", "test2@test.ru")
+    update = api.update_employee_info(id_employee, fake.last_name(), fake.email())
     assert update["id"] == id_employee
-    assert update["email"] == "test2@test.ru"
+    assert update["email"] == fake.email()
     assert update["isActive"] == True
